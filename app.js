b8e7e5e1042175e57edeec35ae6a04d1feba3450
AmanAyoub <aman.ayoub16@gmail.com>
2025-07-20 10:03:24 +0430

    rolls and sides by query string

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 const HTTP = require('http');
 const PORT = 3000;
 const URL = require('url').URL;
-function rollDice(min, max) {
+
+function dieRoll(min, max) {
   max = !max ? 6 : max;
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getParams(path) {
+  let myURL = new URL(path, `http://localhost:${PORT}`);
+  return myURL.searchParams;
+}
+
+function rollDice(params) {
+  let rolls = +params.get('rolls');
+  let sides = +params.get('sides');
+  let body = '';
+
+  do {
+    body += `${dieRoll(1, sides)}\n`;
+    rolls -= 1;
+  } while(rolls > 0);
+
+  return body;
 }
 
 const SERVER = HTTP.createServer((req, res) => {
   let method = req.method;
   let path = req.url;
-  let myURL = new URL(path, 'http://localhost');
-  let rolls = +myURL.searchParams.get('rolls');
-  let sides = +myURL.searchParams.get('sides');
-
+  
   if (path === '/favicon.ico') {
     res.statusCode = 404;
     res.end();
   } else {
+    let content = rollDice(getParams(path));
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    do {
-      let dice = rollDice(1, Number(sides));
-      res.write(`${dice}\n`);
-      rolls -= 1;
-    } while (rolls > 0);
-
+    res.write(content);
     res.write(`${method} ${path}\n`);
     res.end();
   }
