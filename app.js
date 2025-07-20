fd74dd5d443fdae155f36a34275eb99bb21f6efa
AmanAyoub <aman.ayoub16@gmail.com>
2025-07-20 09:53:08 +0430

    rolls and sides

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const HTTP = require('http');
 const PORT = 3000;
+const URL = require('url').URL;
+function rollDice(min, max) {
+  max = !max ? 6 : max;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const SERVER = HTTP.createServer((req, res) => {
   let method = req.method;
   let path = req.url;
+  let myURL = new URL(path, 'http://localhost');
+  let rolls = +myURL.searchParams.get('rolls');
+  let sides = +myURL.searchParams.get('sides');
 
   if (path === '/favicon.ico') {
     res.statusCode = 404;
     res.end();
   } else {
-    let dice = Math.floor(Math.random() * 6) + 1;
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.write(`${dice}\n`);
+    do {
+      let dice = rollDice(1, Number(sides));
+      res.write(`${dice}\n`);
+      rolls -= 1;
+    } while (rolls > 0);
+
     res.write(`${method} ${path}\n`);
     res.end();
   }
